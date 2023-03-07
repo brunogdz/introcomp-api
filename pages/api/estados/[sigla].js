b67359fedb5512f2cc5a80760586eedc4d894bf5
bruno-linux <brunog.vix@gmail.com>
2023-03-06 23:50:46 -0300

    sul done

--- pages/api/estados/[sigla].js
+++ pages/api/estados/[sigla].js
@@ -3,6 +3,10 @@ import dataMG from "../../../estados/sudeste/mg/data"
 import dataRJ from "../../../estados/sudeste/rj/data"
 import dataSP from "../../../estados/sudeste/sp/data"
 
+import dataSC from "../../../estados/sul/sc/data"
+import dataRS from "../../../estados/sul/rs/data"
+import dataPR from "../../../estados/sul/pr/data"
+
 export default function handler(req, res) {
     const {sigla} = req.query
     switch (sigla){
@@ -17,5 +21,15 @@ export default function handler(req, res) {
             break;
         case 'sp' || 'SP':
             res.status(200).json({name: dataSP})
+            break;
+        case 'sc':
+            res.status(200).json({name: dataSC})
+            break;
+        case 'rs':
+            res.status(200).json({name: dataRS})
+            break;
+        case 'pr':
+            res.status(200).json({name: dataPR})
+            break;
     }
 }
